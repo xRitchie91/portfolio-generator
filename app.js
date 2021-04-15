5d1495d04fa2f019da314c346da092880871fef5
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-04-15 03:46:01 -0500

    inquirer initial

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-  
-  // Notice the lack of  paraentheses around the `profileDataArr` parameter?
-  const printProfileData = profileDataArr => {
-      // This
-      for (let i = 0; i < profileDataArr.length; i += 1) {
-          console.log(profileDataArr[i]);
-      }
+const inquirer = require('inquirer');
+console.log(inquirer)
 
-      console.log('================');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-      // Is the same as this...
-      profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//  if (err) throw err;
+
+//  console.log('Portfolio complete! CHeckout index.html to see the output!');
+// });
